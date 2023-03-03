46c2a207fc583a8d7bc1b650111eb50120f1da08
Anshika1610 <71184320+Anshika1610@users.noreply.github.com>
2023-03-03 15:28:15 +0530

    Create FibonacciUsingRecursion.cpp

---
+++ FibonacciUsingRecursion.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n){
+    if(n<=1){
+        return n;
+    }
+    return fibonacci(n-2)+fibonacci(n-1);
+    
+}
+
+int main()
+{
+    cout<<fibonacci(4);
+
+    return 0;
+}
